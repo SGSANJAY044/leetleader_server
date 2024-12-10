bc2e68774e30334bc0ebb889404b5bfa16a48251
sgsanjay044@gmail.com <sanjay.s@surveysparrow.com>
2024-12-10 10:47:32 +0530

    chore: Config Setup GIn

---
+++ internal/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	ServerAddress string `mapstructure:"server_address"`
+	Environment  string `mapstructure:"environment"`
+	Database     DatabaseConfig
+}
+
+type DatabaseConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+	DBName   string `mapstructure:"dbname"`
+}
+
+func LoadConfig() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./configs")
+	viper.AutomaticEnv()
+
+	var config Config
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
